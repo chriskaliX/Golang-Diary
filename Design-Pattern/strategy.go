1dc695f3ebdfbcc04631a7f794c58031bd7ceda3
chriskaliX <chriskali@163.com>
2022-02-04 15:44:02 +0800

    strategy

---
+++ Design-Pattern/strategy.go
@@ -0,0 +1,49 @@
+package designpattern
+
+import (
+	"fmt"
+)
+
+// @Note: 一个类的行为或其算法可以在运行时更改。这种类型的设计模式属于行为型模式
+// @Note: 解决在有多种算法相似的情况下，使用 if...else 所带来的复杂和难以维护
+
+type Payment struct {
+	context  *PaymentContext
+	strategy PaymentStrategy
+}
+
+type PaymentContext struct {
+	Name, CardID string
+	Money        int
+}
+
+func NewPayment(name, cardid string, money int, strategy PaymentStrategy) *Payment {
+	return &Payment{
+		context: &PaymentContext{
+			Name:   name,
+			CardID: cardid,
+			Money:  money,
+		},
+		strategy: strategy,
+	}
+}
+
+func (p *Payment) Pay() {
+	p.strategy.Pay(p.context)
+}
+
+type PaymentStrategy interface {
+	Pay(*PaymentContext)
+}
+
+type Cash struct{}
+
+func (*Cash) Pay(ctx *PaymentContext) {
+	fmt.Printf("Pay $%d to %s by cash", ctx.Money, ctx.Name)
+}
+
+type Bank struct{}
+
+func (*Bank) Pay(ctx *PaymentContext) {
+	fmt.Printf("Pay $%d to %s by bank account %s", ctx.Money, ctx.Name, ctx.CardID)
+}
---
+++ Design-Pattern/strategy_test.go
@@ -0,0 +1,24 @@
+package designpattern
+
+import (
+	"testing"
+)
+
+func ExamplePayByCash() {
+	payment := NewPayment("Ada", "", 123, &Cash{})
+	payment.Pay()
+	// Output:
+	// Pay $123 to Ada by cash
+}
+
+func ExamplePayByBank() {
+	payment := NewPayment("Bob", "0002", 888, &Bank{})
+	payment.Pay()
+	// Output:
+	// Pay $888 to Bob by bank account 0002
+}
+
+func TestStrategy(t *testing.T) {
+	ExamplePayByBank()
+	ExamplePayByCash()
+}
